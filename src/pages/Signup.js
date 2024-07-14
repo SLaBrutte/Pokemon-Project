a97faebc004c69ba47095493fcfcad4a7a6e446a
Samuel LaBrutte <samlabrutte@yahoo.com>
2024-07-13 19:21:59 -0500

    Second Commmit

---
+++ src/pages/Signup.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Signup = (props) => {
+  return (
+    <div>
+        <input type="text" placeholder="Email"></input>
+        <input type="text" placeholder="Confirm Email"></input>
+        <input type="text" placeholder="Password"></input>
+    </div>
+  )
+}
+
+export default Signup
