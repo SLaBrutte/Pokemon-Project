39efd8cb49d442339c35702005367ef04a56b00d
Samuel LaBrutte <samlabrutte@yahoo.com>
2024-07-22 21:57:00 -0500

    Created mock json data & started showing the data onto our GameIndex page

--- src/pages/GameIndex.js
+++ src/pages/GameIndex.js
@@ -1,17 +1,33 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-
+// https://letsreact.org/how-to-fetch-data-from-json-files-in-react-and-display-it-in-a-component/
 
 const GameIndex = (props) => {
+  const [games, setGames] = useState([]);
 
-
-
+  useEffect(() => {
+    axios.get("/Mockdata.json")
+    .then((response) => {
+      setGames(response.data)
+    })
+    .catch((error) => {
+        console.error("Error fetching games:", error)
+    })
+  }, [])
 
   return (
     <div>
       <h1>See our gaming selection</h1>
+      <div>
+        {games.map((game) => (
+          <div key={game.id}>
+            <h2>{game.title}</h2>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default GameIndex
+export default GameIndex;
